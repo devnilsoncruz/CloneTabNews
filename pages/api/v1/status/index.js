7557151e6dabc55ce1d34350081ac9d35cba2cda
Nilson Cruz <146475333+devnilsoncruz@users.noreply.github.com>
2025-07-20 17:36:31 +0000

    add page api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,4 @@
-
 function status(request, response) {
-  response.status(200).json({ chave: "testando uma API" })
+  response.status(200).json({ chave: "testando uma API" });
 }
 export default status
