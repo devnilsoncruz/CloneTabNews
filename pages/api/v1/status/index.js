36b4d0a1d9766f2a0899397ad3a6252cd43d2a07
Nilson Cruz <146475333+devnilsoncruz@users.noreply.github.com>
2025-07-20 16:34:49 +0000

    teste error

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,5 @@
 
-
 function status(request, response) {
-  response.status(200).json({ chave: "testando API  " });
-
+  response.status(200).json({ chave: "testando uma API" })
 }
 export default status
-
--- pages/integration/api/v1/status/get.test.js
+++ pages/integration/api/v1/status/get.test.js
@@ -2,5 +2,7 @@ test("testando um api cod 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
 
+
+
 })
 
